0bf4e12b5cf011c54b7cef6b87e3f36567aa5f46
zbsz <zbigniewo@gmail.com>
2015-05-18 22:54:37 +0200

    Add couple utility methods to CancellableFuture.

--- src/main/scala/com/geteit/concurrent/CancellableFuture.scala
+++ src/main/scala/com/geteit/concurrent/CancellableFuture.scala
@@ -6,7 +6,7 @@ import com.geteit.concurrent.CancellableFuture.CancelException
 
 import scala.concurrent._
 import scala.concurrent.duration.{Duration, FiniteDuration}
-import scala.util.control.NonFatal
+import scala.util.control.{NoStackTrace, NonFatal}
 import scala.util.{Failure, Success, Try}
 
 class CancellableFuture[+A](promise: Promise[A]) extends Awaitable[A] { self =>
@@ -16,6 +16,14 @@ class CancellableFuture[+A](promise: Promise[A]) extends Awaitable[A] { self =>
   def cancel(): Boolean = promise tryFailure CancelException
 
   def onComplete[B](f: Try[A] => B)(implicit executor: ExecutionContext): Unit = future.onComplete(f)
+
+  def onSuccess[U](pf: PartialFunction[A, U])(implicit executor: ExecutionContext): Unit = future.onSuccess(pf)
+
+  def onFailure[U](pf: PartialFunction[Throwable, U])(implicit executor: ExecutionContext): Unit = future.onFailure(pf)
+
+  def onCancelled(body: => Unit)(implicit executor: ExecutionContext): Unit = future.onFailure {
+    case CancelException => body
+  }
 
   def map[B](f: A => B)(implicit executor: ExecutionContext): CancellableFuture[B] = {
     val p = Promise[B]()
@@ -67,6 +75,41 @@ class CancellableFuture[+A](promise: Promise[A]) extends Awaitable[A] { self =>
     }
   }
 
+  def recover[B >: A](pf: PartialFunction[Throwable, B])(implicit executor: ExecutionContext) = recoverWith(pf.andThen(CancellableFuture.successful))
+
+  def recoverWith[U >: A](pf: PartialFunction[Throwable, CancellableFuture[U]])(implicit executor: ExecutionContext): CancellableFuture[U] = {
+    val p = Promise[U]()
+    @volatile var cancelFunc = Option(self.cancel _)
+
+    future.onComplete { res =>
+      cancelFunc = None
+      if (!p.isCompleted) res match {
+        case Failure(t) if pf.isDefinedAt(t) =>
+          val fut = pf.applyOrElse(t, (_: Throwable) => this)
+          cancelFunc = Some(fut.cancel)
+          fut onComplete { res =>
+            cancelFunc = None
+            p tryComplete res
+          }
+          if (p.isCompleted) fut.cancel()
+        case other =>
+          p tryComplete other
+      }
+    }
+    new CancellableFuture(p) {
+      override def cancel(): Boolean = {
+        if (super.cancel()) {
+          Future(cancelFunc.foreach(_.apply()))(CancellableFuture.internalExecutionContext)
+          true
+        } else false
+      }
+    }
+  }
+
+  def flatten[B](implicit executor: ExecutionContext, evidence: A <:< CancellableFuture[B]): CancellableFuture[B] = flatMap(x => x)
+
+  def zip[B](other: CancellableFuture[B])(implicit executor: ExecutionContext): CancellableFuture[(A, B)] = CancellableFuture.zip(self, other)
+
   @throws[InterruptedException](classOf[InterruptedException])
   @throws[TimeoutException](classOf[TimeoutException])
   override def ready(atMost: Duration)(implicit permit: CanAwait): this.type = {
@@ -84,7 +127,7 @@ object CancellableFuture {
 
   lazy val DelayTimer = new java.util.Timer("DelayTimer", true)
 
-  object CancelException extends RuntimeException("Operation cancelled")
+  case object CancelException extends RuntimeException("Operation cancelled") with NoStackTrace
 
   class PromiseCompletingRunnable[T](body: => T) extends Runnable {
     val promise = Promise[T]()
@@ -117,18 +160,56 @@ object CancellableFuture {
   }
 
   def delay(d: FiniteDuration): CancellableFuture[Unit] = {
-    val p = Promise[Unit]()
-    val task = new TimerTask {
-      override def run(): Unit = p.trySuccess({})
+    if (d <= Duration.Zero) successful(())
+    else {
+      val p = Promise[Unit]()
+      val task = new TimerTask {
+        override def run(): Unit = p.trySuccess({})
+      }
+      p.future.onFailure { case CancelException => task.cancel() } (internalExecutionContext)
+      DelayTimer.schedule(task, d.toMillis)
+      new CancellableFuture(p)
     }
-    p.future.onFailure { case CancelException => task.cancel() } (internalExecutionContext)
-    DelayTimer.schedule(task, d.toMillis)
-    new CancellableFuture(p)
   }
 
-  def delayed[A](d: FiniteDuration, body: => A)(implicit executor: ExecutionContext) = delay(d) map { _ => body }
+  def delayed[A](d: FiniteDuration)(body: => A)(implicit executor: ExecutionContext) = delay(d) map { _ => body }
 
   def successful[A](result: A): CancellableFuture[A] = new CancellableFuture[A](Promise.successful(result))
 
   def failed[A](cause: Throwable): CancellableFuture[A] = new CancellableFuture[A](Promise.failed(cause))
+
+  def cancelled[A](): CancellableFuture[A] = failed(CancelException)
+
+  def sequence[A](in: Seq[CancellableFuture[A]])(implicit executor: ExecutionContext): CancellableFuture[Seq[A]] =
+    in.foldLeft(successful(Seq.newBuilder[A])) {
+      (fr, fa) => for (r <- fr; a <- fa) yield r += a
+    } map (_.result())
+
+  def traverse[A, B](in: Seq[A])(f: A => CancellableFuture[B])(implicit executor: ExecutionContext): CancellableFuture[Seq[B]] = sequence(in.map(f))
+
+  def traverseSequential[A, B](in: Seq[A])(f: A => CancellableFuture[B])(implicit executor: ExecutionContext): CancellableFuture[Seq[B]] = {
+    def processNext(remaining: Seq[A], acc: List[B] = Nil): CancellableFuture[Seq[B]] =
+      if (remaining.isEmpty) CancellableFuture.successful(acc.reverse)
+      else f(remaining.head) flatMap { res => processNext(remaining.tail, res :: acc) }
+
+    processNext(in)
+  }
+
+  def zip[A, B](f1: CancellableFuture[A], f2: CancellableFuture[B])(implicit executor: ExecutionContext): CancellableFuture[(A, B)] = {
+    val p = Promise[(A, B)]()
+
+    p.tryCompleteWith((for (r1 <- f1; r2 <- f2) yield (r1, r2)).future)
+
+    new CancellableFuture(p) {
+      override def cancel(): Boolean = {
+        if (super.cancel()) {
+          Future {
+            f1.cancel()
+            f2.cancel()
+          }(CancellableFuture.internalExecutionContext)
+          true
+        } else false
+      }
+    }
+  }
 }
---
+++ src/main/scala/com/geteit/concurrent/Threading.scala
@@ -0,0 +1,16 @@
+package com.geteit.concurrent
+
+import android.os.{Looper, Handler}
+import com.geteit.util.Log
+
+import scala.concurrent.ExecutionContext
+
+object Threading {
+  implicit val global: ExecutionContext = LimitedExecutionContext.CpuBoundExecutor
+
+  implicit val ui: ExecutionContext = new ExecutionContext {
+    val handler = new Handler(Looper.getMainLooper)
+    override def reportFailure(cause: Throwable): Unit = Log.error("failure reported", cause)("Threading.ui")
+    override def execute(runnable: Runnable): Unit = handler.post(runnable)
+  }
+}
