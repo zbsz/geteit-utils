f8ecd9d82361bbd9891c0e524f60d0797ca8ccab
zbsz <zbigniewo@gmail.com>
2016-01-09 15:16:45 +0100

    Rewrite PreferenceSignal. Chaned to always use default shared prefarences. Added global map of preference signals, updated in sigle place.

--- src/main/scala/com/geteit/util/PreferenceSignal.scala
+++ src/main/scala/com/geteit/util/PreferenceSignal.scala
@@ -3,14 +3,18 @@ package com.geteit.util
 import android.content.SharedPreferences.{Editor, OnSharedPreferenceChangeListener}
 import android.content.{Context, SharedPreferences}
 import android.preference.PreferenceManager
+import com.geteit.app.GtApplication
 import com.geteit.concurrent.{LimitedExecutionContext, Threading}
 import com.geteit.events._
+import com.geteit.util.Log._
+import com.geteit.util._
 import com.geteit.util.PreferenceSignal.Helper
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
 
 object PreferenceSignal {
+  private implicit val Tag: LogTag = "PreferenceSignal"
 
   trait Helper[T] {
     def read(prefs: SharedPreferences, key: String, default: T): T
@@ -37,61 +41,40 @@ object PreferenceSignal {
     def write(editor: Editor, key: String, value: Boolean) { editor.putBoolean(key, value) }
   }
 
-  def apply[T](key: String, default: T)(implicit context: Context, helper: Helper[T]) = new PreferenceSignal[T](key, PreferenceManager.getDefaultSharedPreferences(context), default)
+  private val signals = new scala.collection.mutable.HashMap[String, PreferenceSignal[_]]
+
+  private lazy val prefs = returning(PreferenceManager.getDefaultSharedPreferences(GtApplication.APP_INSTANCE)) { prefs =>
+    prefs.registerOnSharedPreferenceChangeListener(new OnSharedPreferenceChangeListener {
+      def onSharedPreferenceChanged(sharedPreferences: SharedPreferences, k: String) = signals.get(k) foreach (_.reload())
+    })
+  }
+
+  def apply[T](key: String, default: T)(implicit helper: Helper[T]) =
+    signals.getOrElseUpdate(key, new PreferenceSignal[T](key, prefs, default)).asInstanceOf[PreferenceSignal[T]]
 
   private implicit val PreferenceExecutionContext = new LimitedExecutionContext(1, Threading.io)
 
   private val SaveQueue = new ThrottledProcessingQueue[PreferenceSignal[_]](250.millis, { signals => Future {
-    signals.distinct.groupBy(_.prefs) foreach {
-      case (prefs, ss) =>
-        val editor = prefs.edit()
-        ss foreach { _.writeCurrent(editor) }
-        editor.commit()
+    verbose(s"saving preference signal values: $signals")
+    signals foreach { signal =>
+      val editor = prefs.edit()
+      signal.writeCurrent(editor)
+      editor.commit()
     }
   }})
 }
 
-class PreferenceSignal[T](private val key: String, preferences: => SharedPreferences, private val default: T)(implicit val helper: Helper[T]) extends SourceSignal[T] with UiEventSource {
+class PreferenceSignal[T] private (key: String, preferences: => SharedPreferences, default: T)(implicit val helper: Helper[T]) extends SourceSignal[T] with UiEventSource {
   import PreferenceSignal._
   private lazy val prefs = preferences
 
   disableAutowiring()
 
-  private val listener = new OnSharedPreferenceChangeListener {
-    def onSharedPreferenceChanged(sharedPreferences: SharedPreferences, k: String) {
-      if (key == k) Future {
-        PreferenceSignal.super.publish(read)
-      }
-    }
-  }
+  onChanged { _ => SaveQueue ! this } (EventContext.Global)
 
-  Future {
-    publish(read)
-  }
+  reload()
 
-  private def read = helper.read(prefs, key, default)
-
-  def readPrefValue = Future { read }
+  private def reload() = Future { publish(helper.read(prefs, key, default), PreferenceExecutionContext) }
 
   private def writeCurrent(editor: SharedPreferences.Editor): Unit = helper.write(editor, key, currentValue.getOrElse(default))
-
-  def update(f: T => T) = Future { publish(f(read)) }
-
-  override def publish(event: T): Unit = {
-    if (value != event) {
-      super.publish(event)
-
-      SaveQueue ! this
-    }
-  }
-
-  override protected def onWire(): Unit = Future {
-    prefs.registerOnSharedPreferenceChangeListener(listener)
-    super.publish(read)
-  }
-
-  override protected def onUnwire() = Future {
-    prefs.unregisterOnSharedPreferenceChangeListener(listener)
-  }
 }
-
